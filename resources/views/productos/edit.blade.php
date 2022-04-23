a16906f08cb689f8472d3f97366fad7412d992dd
JsMejia21 <banosmj21@gmail.com>
2022-04-23 16:57:19 -0600

    Laboratorio No 2, Tecno. Emergentes

---
+++ resources/views/productos/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">{{ __('Actualizar Datos') }}</div>
+                <div class="card-body">
+                    <form method = "post" action = " {{ url('/productos/'.$productos->id.'/edit')}}">
+                        @csrf
+                         {{ method_field('POST') }}
+                         @include('productos.formedit')
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
